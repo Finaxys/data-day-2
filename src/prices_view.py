c408d56fa25a2a010f278912ae84de09b79f2e4a
Fabien CHERER <fcherer@finaxys.com>
2023-07-06 16:15:47 +0200

    [SPEED] Corrections Théo

--- src/prices_view.py
+++ src/prices_view.py
@@ -10,25 +10,33 @@ def create_snowpark_session():
 
 
 session = create_snowpark_session()
-silver_price = session.sql("SELECT * FROM SILVER_PRICE_ANALYTICS WHERE NAME = 'AXA'").to_pandas()
+silver_price = session.table("SILVER_PRICE_ANALYTICS").to_pandas()
 
-final_df = silver_price.filter(['TIMESTAMP', 'PRICE'])
 
-final_df['TIMESTAMP'] = pd.to_datetime(final_df['TIMESTAMP'] / 1000, unit='ms').astype('datetime64[ms]')
+def get_x_y_entity(val, time_val):
+    final_df = silver_price[silver_price['NAME'] == val].filter(['TIMESTAMP', 'PRICE'])
 
-final_df = final_df.groupby('TIMESTAMP', as_index=False, sort=False)['PRICE'].mean()
+    final_df['TIMESTAMP'] = pd.to_datetime(final_df['TIMESTAMP'] / 1000, unit='ms').astype('datetime64[ms]')
 
-final_df = final_df.sort_values('TIMESTAMP')
-final_df = final_df.set_index('TIMESTAMP')
-final_df = final_df.resample('ms').ffill()
-final_df = final_df.resample('30T').mean()
+    final_df = final_df.groupby('TIMESTAMP', as_index=False, sort=False)['PRICE'].mean()
 
-final_df = final_df.between_time('07:00:00', '15:00:00')
+    final_df = final_df.sort_values('TIMESTAMP')
 
-final_df = final_df.reset_index()
-final_df['TIMESTAMP'] = final_df['TIMESTAMP'].astype(str)
-final_df = final_df.set_index('TIMESTAMP')
+    final_df.TIMESTAMP = final_df.TIMESTAMP.dt.floor('30T').ffill()
+    final_df = final_df.groupby('TIMESTAMP')['PRICE'].mean()
 
+    final_df = final_df.between_time('07:00:00', '15:00:00')
+
+    final_df = final_df.reset_index()
+    final_df['TIMESTAMP'] = final_df['TIMESTAMP'].astype(str)
+    final_df = final_df.set_index('TIMESTAMP')
+
+    return final_df
+
+
+entity = 'AXA'
+time_val ='30'
+final_df = get_x_y_entity(entity, time_val)
 plt.plot(final_df)
 plt.title('Price evolution')
 plt.xlabel('TIMESTAMP')
